c88b0b240f2f1bce9bacbc11260c2abc1d744dac
fuga296 <fu121gam1729@gmail.com>
2025-06-15 23:05:19 +0900

    Active GitHub Actions and Create Home

--- 01_2025-05-01/001_murder-mystery-manager_2025-05-01/src/home/components/Header.jsx
+++ 01_2025-05-01/001_murder-mystery-manager_2025-05-01/src/home/components/Header.jsx
@@ -2,7 +2,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-// import logo from '../../assets/logo.png';
+import logo from '../../assets/logo.png';
 
 function Header() {
   return (
@@ -10,7 +10,7 @@ function Header() {
       <Box sx={{ flexGrow: 1, borderBottom: "1px solid rgb(180, 180, 180)", mb: 5 }}>
         <AppBar position="static" elevation={0} sx={{ backgroundColor: '#fff', color: 'black' }}>
           <Toolbar>
-            {/* <img src={logo} alt='logo' width={50} /> */}
+            <img src={logo} alt='logo' width={50} />
             <Typography variant="h6" component="div" sx={{ flexGrow: 1, ml: 3 }}>
               Muder Mystery Manager
             </Typography>
